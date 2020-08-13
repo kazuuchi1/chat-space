9e7ec407a57fa6d9675112fc07b84c824cff31da
kazuuchi1 <68491740+kazuuchi1@users.noreply.github.com>
2020-08-13 18:37:10 +0900

    chat-space 修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,10 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  	@group = Group.find(params[:id])
+  end
+
     def update
       @group = Group.find(params[:id])
       if @group.update(group_params)
