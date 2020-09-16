28b0964e257d8142e27365f946f76e7b7937290e
kazuuchi1 <68491740+kazuuchi1@users.noreply.github.com>
2020-09-16 12:57:47 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
- server "54.249.184.203", user: "ec2-user", roles: %w{db}
+ server "54.249.184.203", user: "ec2-user", roles: %w{app db web}
 
 
 
