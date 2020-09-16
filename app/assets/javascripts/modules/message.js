ce0e1c6aaff4bcd9d237a9bfa9a612e11da0e313
kazuuchi1 <68491740+kazuuchi1@users.noreply.github.com>
2020-09-16 20:09:59 +0900

    デプロイ準備中

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,33 +2,31 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-      `<div class="MessageBox" data-message-id=${message.id}>
-        <div class="message">
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-meesage">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-            <img class="Message__image" src="${message.image}">
-          </div>
-        </div>`
-      return html;
-    } else {
-      let html =
-      `<div class="MessageBox" data-message-id=${message.id}>
-       <div class="message">
+      `<div class="message" data-message-id=${message.id}>
         <div class="upper-message">
           <div class="upper-message__user-name">
             ${message.user_name}
           </div>
-          <div class="Mupper-message__date">
+          <div class="upper-message__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-meesage">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+          <img class="Message__image" src="${message.image}">
+        </div>
+      </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
             ${message.created_at}
           </div>
         </div>
@@ -37,7 +35,7 @@ $(function(){
             ${message.content}
           </p>
         </div>
-       </div>`
+      </div>`
       return html;
     };
   }
